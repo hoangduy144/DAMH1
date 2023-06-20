340ec07d967d3b0e59204358b2fa3b36b449ef8f
hoangduy144 <hoangduy8741@gmail.com>
2023-06-20 19:05:56 +0700

    Fix Dictionary

--- src/model/Dict.java
+++ src/model/Dict.java
@@ -1,55 +1,30 @@
 package model;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Random;
 
-import java.io.*;
-import java.util.*;
-
 public class Dict {
+	///////////////Tham khảo trên Internet nhưng tự code lại theo tìm hiểu của bản thân////////////////
     private HashMap<String, LinkedList<String>> dictionary;
-    private LinkedList<String> historyList, deleteList;
-    private LinkedHashMap<String, LinkedList<String>> modifiedList;
+    private LinkedList<String> historyList;
 
     public Dict() {
         dictionary = new HashMap<>();
         historyList = new LinkedList<>();
-        deleteList = new LinkedList<>();
-        modifiedList = new LinkedHashMap<>();
 
         loadMap(dictionary, "slang.txt");
         loadLinkedList(historyList, "history.txt");
-        loadLinkedList(deleteList, "delete.txt");
-
-        for (String slang : deleteList) {
-            dictionary.remove(slang);
-        }
-
-        loadMap(modifiedList, "modified.txt");
-
-        for (Map.Entry<String, LinkedList<String>> entry : modifiedList.entrySet()) {
-            String key = entry.getKey();
-            LinkedList<String> val = entry.getValue();
-            dictionary.put(key, val);
-        }
     }
 
     private void loadMap(Map<String, LinkedList<String>> map, String filename) {
-        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(filename)))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split("`");
@@ -66,7 +41,7 @@ public class Dict {
     }
 
     private void loadLinkedList(LinkedList<String> list, String filename) {
-        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(filename)))) {
             String line;
             while ((line = br.readLine()) != null) {
                 list.add(line);
@@ -75,42 +50,8 @@ public class Dict {
             e.printStackTrace();
         }
     }
-
-    private void saveLinkedList(LinkedList<String> list, String filename) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
-            for (String item : list) {
-                bw.write(item);
-                bw.newLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void saveModified() {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("modified.txt"))) {
-            for (Map.Entry<String, LinkedList<String>> entry : modifiedList.entrySet()) {
-                String key = entry.getKey();
-                LinkedList<String> val = entry.getValue();
-                StringBuilder sb = new StringBuilder();
-                for (String definition : val) {
-                    sb.append(definition).append("|");
-                }
-                sb.deleteCharAt(sb.length() - 1);
-                bw.write(key + "`" + sb.toString());
-                bw.newLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void save() {
-        saveLinkedList(deleteList, "delete.txt");
-        saveLinkedList(historyList, "history.txt");
-        saveModified();
-    }
-
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    
     public void addHistory(String history) {
         historyList.add(history);
     }
@@ -127,14 +68,12 @@ public class Dict {
         LinkedList<String> definitions = dictionary.getOrDefault(slang, new LinkedList<>());
         definitions.add(definition);
         dictionary.put(slang, definitions);
-        modifiedList.put(slang, definitions);
     }
 
     public void addNew(String slang, String definition) {
         LinkedList<String> definitions = new LinkedList<>();
         definitions.add(definition);
         dictionary.put(slang, definitions);
-        modifiedList.put(slang, definitions);
     }
 
     public boolean hasSlang(String slang) {
@@ -147,7 +86,6 @@ public class Dict {
             int index = definitions.indexOf(oldDefinition);
             if (index != -1) {
                 definitions.set(index, newDefinition);
-                modifiedList.put(slang, definitions);
                 return true;
             }
         }
@@ -155,10 +93,10 @@ public class Dict {
     }
 
     public void deleteSlang(String slang) {
-        deleteList.add(slang);
         dictionary.remove(slang);
     }
 
+    ///////////////Tham khảo trên Internet nhưng tự code lại theo tìm hiểu của bản thân////////////////
     public ArrayList<String> searchDefinition(String definition) {
         ArrayList<String> slangs = new ArrayList<>();
         for (Map.Entry<String, LinkedList<String>> entry : dictionary.entrySet()) {
@@ -173,6 +111,7 @@ public class Dict {
         }
         return slangs;
     }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public String randomSlang() {
         Random random = new Random();
@@ -180,6 +119,7 @@ public class Dict {
         return slangs.get(random.nextInt(slangs.size()));
     }
 
+    ///////////////Tham khảo trên Internet nhưng tự code lại theo tìm hiểu của bản thân////////////////
     public HashMap<String, LinkedList<String>> slangGame() {
         HashMap<String, LinkedList<String>> game = new HashMap<>();
         Random random = new Random();
@@ -190,27 +130,11 @@ public class Dict {
         }
         return game;
     }
-
-    public HashMap<String, ArrayList<String>> definitionGame() {
-        HashMap<String, ArrayList<String>> game = new HashMap<>();
-        Random random = new Random();
-        ArrayList<String> slangs = new ArrayList<>(dictionary.keySet());
-        while (game.size() < 4) {
-            String slang = slangs.get(random.nextInt(slangs.size()));
-            LinkedList<String> definitions = dictionary.get(slang);
-            String definition = definitions.get(random.nextInt(definitions.size()));
-            ArrayList<String> slangsContainingDef = searchDefinition(definition);
-            game.put(definition, slangsContainingDef);
-        }
-        return game;
-    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public void reset() {
         dictionary.clear();
-        modifiedList.clear();
         historyList.clear();
-        deleteList.clear();
         loadMap(dictionary, "slang.txt");
-        loadMap(modifiedList, "modified.txt");
     }
 }
--- src/test/SlangWordApp.java
+++ src/test/SlangWordApp.java
@@ -5,25 +5,22 @@ import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 
 import ui.SlangApp;
+
 public class SlangWordApp {
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			
-			@Override
-			public void run() {
-				try {
-				    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-				        if ("Nimbus".equals(info.getName())) {
-				            UIManager.setLookAndFeel(info.getClassName());
-				            break;
-				        }
-				    }
-				} catch (Exception e) {
-				}
-				new SlangApp();
-			}
-		});
-	}
-
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
+                        UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            new SlangApp();
+        });
+    }
 }
